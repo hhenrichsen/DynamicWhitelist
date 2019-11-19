b551b0991b6c4d61474e6599f1b5d804ea1a4462
Alvin Vrolijk <alvin@funenzo.yt>
2019-11-19 10:57:01 +0100

    .gitignore change
    Added .idea and target folder

---
+++ src/main/java/nl/alvinvrolijk/dynamicwhitelist/DynamicWhitelist.java
@@ -0,0 +1,17 @@
+package nl.alvinvrolijk.dynamicwhitelist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DynamicWhitelist extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
